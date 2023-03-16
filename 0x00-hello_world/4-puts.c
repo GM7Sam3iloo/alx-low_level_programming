692c0969b1fdc29b452b882a33686dc0e479a0c1
GM7Sam3iloo <am9259710@gmail.com>
2023-03-16 13:40:09 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main function
- * returns 0
+ * 
+ * returns: always returns 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
